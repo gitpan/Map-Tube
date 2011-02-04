7ca5fd3fd418b92efdc78d74a4823328f6bd06b4
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2011-02-04 19:03:55 +0000

    Import of MANWAR/Map-Tube-2.13 from CPAN.
    gitpan-cpan-distribution: Map-Tube
    gitpan-cpan-version:      2.13
    gitpan-cpan-path:         MANWAR/Map-Tube-2.13.tar.gz
    gitpan-cpan-author:       MANWAR
    gitpan-cpan-maturity:     released

--- lib/Map/Tube.pm
+++ lib/Map/Tube.pm
@@ -14,7 +14,7 @@ Map::Tube - A very simple perl interface to the London Tube Map.
 
 =head1 VERSION
 
-Version 2.12
+Version 2.13
 
 =head1 AWARD
 
@@ -24,7 +24,7 @@ http://download.famouswhy.com/map_tube/
 
 =cut
 
-our $VERSION = '2.12';
+our $VERSION = '2.13';
 
 
 =head1 SYNOPSIS
